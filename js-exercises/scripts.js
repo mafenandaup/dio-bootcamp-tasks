d8d99d2e4e452e1429bbf461c973e006bb5a119f
Maria Fernanda <mafenandaup@gmail.com>
2025-06-16 21:45:30 -0300

    refactor: add new tasks

--- js-exercises/scripts.js
+++ js-exercises/scripts.js
@@ -11,7 +11,7 @@ function avgNumArray(arr) {
         sum += arr[i];
     }
     const resultado = Math.floor(sum / arr.length);
-    return (`A média do array é ` +resultado)
+    return (`A média do array é ` + resultado)
 }
 
 function maxNumArray(arr) {
@@ -41,3 +41,72 @@ const menornum = minNumArray(arrNumbers)
 console.log(maiornum)
 console.log(menornum)
 console.log(media)
+
+// Exerício 2: tabuada (também com array)
+
+let arrTabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function tabuada(arr, num) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`${num} * ${arr[i]} = ${num * arr[i]}`)
+    }
+}
+
+const resultadoTabuada = tabuada(arrTabuada, 3)
+
+console.log(resultadoTabuada)
+
+// Exercício 3: soma de números pares
+
+function numPares() {
+    let sum = 0;
+    for (let i = 0; i <= 100; i++) {
+        if (i % 2 == 0) {
+            sum += i
+        }
+    }
+    return (`a soma dos números pares é ${sum}`)
+}
+
+const resultPares = numPares();
+console.log(resultPares)
+
+// Exercício 4; inversão de array
+
+function inversionArray(arr, num) {
+    newArr = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArr.push(arr[i])
+    }
+    return newArr
+}
+
+const inverted = inversionArray(arrNumbers)
+
+console.log(inverted)
+
+// Exercício 5: soma de quadrados
+
+function sumSquares(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i] * arr[i];
+    }
+    return (`a soma dos quadrados dos elementos é ${Math.floor(sum)}`)
+}
+
+const somaquadrados = sumSquares(arrNumbers)
+console.log(somaquadrados)
+//Exercício 6: busca por indice
+
+function indexSearch(arr, num) {
+    for (let i = 0; i < arr.length; i++) {
+        if (num == arr[i]) {
+            return (`número encontrado! ${num}`)
+        }
+    }
+    return -1
+}
+
+const busca = indexSearch(arrNumbers, 48)
+console.log(busca)
