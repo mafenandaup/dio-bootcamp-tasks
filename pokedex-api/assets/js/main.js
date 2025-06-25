3cc321716127333bb3711e91aa3133b0aee0bd99
Maria Fernanda <mafenandaup@gmail.com>
2025-06-25 10:47:34 -0300

    refactor: dynamic li functions

--- pokedex-api/assets/js/main.js
+++ pokedex-api/assets/js/main.js
@@ -3,10 +3,9 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToHtml(pokemon) {
-    return `
-            <li class="pokemon"><span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+function convertPokemonToLi(pokemon) {
+    return `<li class="pokemon"><span class="number">#001</span>
+                <span class="name">"${pokemon.name}"</span>
                 <div class="detail">
 
                     <ol class="types">
@@ -16,10 +15,10 @@ function convertPokemonToHtml(pokemon) {
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
                         alt="${pokemon.name} sprite">
                 </div>
-            </li>
-            
-            `
+            </li>`
 }
+
+const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)  // FETCH retorna uma promise ("promessa" de um response conforme executa a busca, se der tudo certo)
     .then((response) => response.json()) //quando o recurso (url) for disponibilizado, o then é chamado.
@@ -28,10 +27,10 @@ fetch(url)  // FETCH retorna uma promise ("promessa" de um response conforme exe
 
     .then((responseBody) => responseBody.results)// aqui já retorna o res convertido 
     // //na vida real não é boa prática ter tantos callbacks, aqui é só pra ser mais dinâmico
-    .then((pokemonList) => {
-        for (let i = 0; pokemonList.lenght; i++) {
-            const pokemon = pokemonList[i]
-            console.log(pokemon)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i]
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
         }
     })
     .catch((error) => console.error(error))
