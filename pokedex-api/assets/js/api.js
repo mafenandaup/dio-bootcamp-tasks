3dfc14e90bda2f5c3d5bae311a6df5941706b734
Maria Fernanda <mafenandaup@gmail.com>
2025-06-25 13:36:43 -0300

    feat: dynamically load pokemons

--- pokedex-api/assets/js/api.js
+++ pokedex-api/assets/js/api.js
@@ -1,14 +1,36 @@
 const pokeApi = {} // objeto representando a pokeApi
 
-pokeApi.getFilteredPokemons = function (offset = 0, limit = 10) {
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getFilteredPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)  // FETCH retorna uma promise ("promessa" de um response conforme executa a busca, se der tudo certo)
+       return fetch(url)  // FETCH retorna uma promise ("promessa" de um response conforme executa a busca, se der tudo certo)
         .then((response) => response.json()) //quando o recurso (url) for disponibilizado, o then é chamado.
         // THEN tem uma função com a respose como parâmetro. Quando ele terminar a req. o THEN será chamado e vai passar essa função dentro das chaves.
         .then((responseBody) => responseBody.results)// aqui já retorna o res convertido 
-       .catch((error) => {
-            console.error('Erro ao buscar Pokémon:', error);
-            return []; // Retorna um array vazio em caso de erro para evitar falhas no resto do código.
-        });
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))//mapeando cada pokemon e "buscando seus detalhes" na função.
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) =>  pokemonDetails)
+
 };
--- pokedex-api/assets/js/main.js
+++ pokedex-api/assets/js/main.js
@@ -3,29 +3,26 @@ const pokemonList = document.getElementById('pokemonList')
 
 
 function convertPokemonToLi(pokemon) {
-    return `<li class="pokemon"><span class="number">#001</span>
-                <span class="name">"${pokemon.name}"</span>
-                <div class="detail">
+    return `<li class="pokemon ${pokemon.type}">
+            <span class="number">#0${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-                    <ol class="types">
-                        <li class="type">poison</li>
-                        <li class="type">grass</li>
-                    </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-                        alt="${pokemon.name} sprite">
-                </div>
-            </li>`
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>`
 }
 
 
 pokeApi.getFilteredPokemons().then((pokemons = []) => {// faz a chadmada do fetch no outro arquivo, auxiliando a organizar melhor o código. a partir daí ele já faz também a chamada da arrow function pra converter cada pokemon em LI.
-
-
-    const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon))
-
-    const newHtml = newList.join('') // junta todos os elementos da lista em uma string, nesse caso sem separador; por default, se coloca uma vírgula
-    pokemonList.innerHTML += newHtml
-    console.log(newList)
+    // junta todos os elementos da lista em uma string, nesse caso sem separador; por default, se coloca uma vírgula
+    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    pokemonList.innerHTML = newHtml
 })
 
     .finally(function () {
