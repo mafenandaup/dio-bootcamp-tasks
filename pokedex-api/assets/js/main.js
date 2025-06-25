68ab76334422ac755d7d10d2a71616e78f7e83b4
Maria Fernanda <mafenandaup@gmail.com>
2025-06-25 14:04:49 -0300

    perf: finish pokedex

--- pokedex-api/assets/js/main.js
+++ pokedex-api/assets/js/main.js
@@ -1,5 +1,9 @@
+const maxRecords = 151 // quantia max. de pokemons que podem aparecer na pag.
+const limit = 10
+let offset = 0;
 
 const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
 
 function convertPokemonToLi(pokemon) {
@@ -18,14 +22,33 @@ function convertPokemonToLi(pokemon) {
         </li>`
 }
 
+function loadPokemonItems(offset, limit) { // estabelece um ponto de partida e um limite para o carregamento de pokemons
+    pokeApi.getFilteredPokemons(offset, limit)
+        .then((pokemons = []) => {// faz a chadmada do fetch no outro arquivo, auxiliando a organizar melhor o código. a partir daí ele já faz também a chamada da arrow function pra converter cada pokemon em LI.
+            // junta todos os elementos da lista em uma string, nesse caso sem separador; por default, se coloca uma vírgula
+            const newHtml = pokemons.map(convertPokemonToLi).join('');
+            pokemonList.innerHTML += newHtml;
 
-pokeApi.getFilteredPokemons().then((pokemons = []) => {// faz a chadmada do fetch no outro arquivo, auxiliando a organizar melhor o código. a partir daí ele já faz também a chamada da arrow function pra converter cada pokemon em LI.
-    // junta todos os elementos da lista em uma string, nesse caso sem separador; por default, se coloca uma vírgula
-    const newHtml = pokemons.map(convertPokemonToLi).join('');
-    pokemonList.innerHTML = newHtml
-})
+        })
+}
 
-    .finally(function () {
-        console.log('Requisição concluída') // executa o bloco de código independente do sucesso ou fracasso da requisição.
-    })
+loadPokemonItems(offset, limit);
 
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordsWithNextPage = offset + limit;
+
+    if (qtdRecordsWithNextPage >= maxRecords) { // enquanto a quantia de registros no display for menor que a máxima
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton); // remove o botão quando terminar
+    } else {
+        loadPokemonItems(offset, limit);
+    }
+});
+
+
+
+//     .finally(function () {
+//     console.log('Requisição concluída') // executa o bloco de código independente do sucesso ou fracasso da requisição.
+// })
